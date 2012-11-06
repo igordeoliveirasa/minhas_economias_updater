0c1fbbd98db183a8457cafe60e0bbd7b5ffa3637
Igor de Oliveira Sá <igor.sa@iMac1.local>
2012-11-06 18:19:27 -0200

    disabled  month option

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 
     bb_navigator = banco_do_brasil_navigator.Navigator()
     bb_navigator.execute_login(arguments.bank_branch, arguments.bank_account, arguments.bank_password)
-    transactions_file_path = bb_navigator.export_transactions(arguments.download_directory_path, 1, bb_navigator.MONTH_AUG)
+    transactions_file_path = bb_navigator.export_transactions(arguments.download_directory_path, 1 ) # bb_navigator.MONTH_AUG) do it later
     bb_navigator.close()
 
     minhas_economias_navigator = minhas_economias_navigator.Navigator()
